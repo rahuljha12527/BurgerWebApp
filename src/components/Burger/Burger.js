82c809656b197079ccd18da98426f2bd5dc89aa5
rahuljha661 <rahuljha12527@gmail.com>
2021-03-11 21:48:40 +0530

    add a dynamic ingredient

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classes from './BurgerIngredient.css';
+
+const burgerIngredient=(props)=>{
+       let ingredient=null;
+
+       switch(props.type){
+           case ('bread-bottom'):
+               ingredient=<div className={classes.BreadBottom}></div>;
+               break;
+           case ('bread-top');
+               ingredient=(
+                   <div className={classes.BreadTop}>
+                       
+                   </div>
+               )    
+       }
+};
+
+export default burgerIngredient;
